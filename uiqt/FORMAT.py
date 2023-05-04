761964ebada95ca2de1600105d8f21f76b215d35
zhouchenhua <noatnu@foxmail.com>
2023-05-04 19:06:40 +0800

    修改

--- uiqt/FORMAT.py
+++ uiqt/FORMAT.py
@@ -50,6 +50,7 @@ class MyWindow(QWidget):
 
         btn3.clicked.connect(self.clearQTextEdit)
         btn1.clicked.connect(self.camel_to_snake)
+        btn2.clicked.connect(self.to_lower_camel)
 
         bQHBoxLayout.addWidget(btn1)
         # bQHBoxLayout.addStretch(1)
@@ -72,6 +73,15 @@ class MyWindow(QWidget):
         else:
             name = reUtils.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
             str1 = reUtils.sub('([a-z0-9])([A-Z])', r'\1_\2', name).lower()
+            self.codeMirrorResult.setPlainText(str1)
+    def to_lower_camel(self):
+        """下划线转小驼峰法命名"""
+        name = self.codeMirror.toPlainText()
+        if len(name) == 0 or len(name) == 0:
+            msg_box = QMessageBox(QMessageBox.Information, '提示', '没有输入字符串')
+            msg_box.exec_()
+        else:
+            str1 = reUtils.sub('_([a-zA-Z])', lambda m: (m.group(1).upper()), name.lower())
             self.codeMirrorResult.setPlainText(str1)
 
 
